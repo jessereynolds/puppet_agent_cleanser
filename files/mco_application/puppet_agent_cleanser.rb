afc573844f0b1a3062b999141665686221dd7574
Jesse Reynolds <jesse@va.com.au>
2017-06-20 13:28:40 +1000

    fully implement application

--- files/mco_application/puppet_agent_cleanser.rb
+++ files/mco_application/puppet_agent_cleanser.rb
@@ -1,12 +1,39 @@
-class MCollective::Application::PuppetAgentCleanser < MCollective::Application
-   description "Cleanse the run state of your Puppet agents"
+class MCollective::Application::Puppet_agent_cleanser < MCollective::Application
+  description "Cleanse the run state of your Puppet agents"
 
-   def main
-      mc = rpcclient("puppet_agent_cleanser")
+  def post_option_parser(configuration)
+    if ARGV.length >= 1
+      configuration[:command] = ARGV.shift
 
-      printrpc mc.echo(:msg => configuration[:message], :options => options)
+      unless ["status", "cleanse"].include?(configuration[:command])
+        raise "Unknown command"
+      end
+    else
+      raise "No command found"
+    end
+  end
 
-      printrpcstats
-   end
+  def status_command
+    mc = rpcclient("puppet_agent_cleanser")
+    printrpc mc.status()
+    printrpcstats
+  end
+
+  def cleanse_command
+    mc = rpcclient("puppet_agent_cleanser")
+    printrpc mc.cleanse()
+    printrpcstats
+  end
+
+  def main
+    impl_method = "%s_command" % configuration[:command]
+
+    if respond_to?(impl_method)
+      send(impl_method)
+    else
+      raise_message(6, configuration[:command])
+    end
+  end
+
 end
 
